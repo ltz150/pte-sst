8b5357c0d5eacaf080f4ce007f6b27bd72362ab3
TZ L <tzl@TZdeMac-mini.local>
2026-05-11 01:31:21 +0800

    Polyfill PDF canvas globals

--- lib/wfd/pdf.ts
+++ lib/wfd/pdf.ts
@@ -1,4 +1,11 @@
+import { DOMMatrix, ImageData, Path2D } from "@napi-rs/canvas";
 import { PDFParse } from "pdf-parse";
+
+const canvasGlobals = globalThis as Record<string, unknown>;
+
+canvasGlobals.DOMMatrix ??= DOMMatrix;
+canvasGlobals.ImageData ??= ImageData;
+canvasGlobals.Path2D ??= Path2D;
 
 export async function extractPdfText(data: Uint8Array) {
   const parser = new PDFParse({ data });
